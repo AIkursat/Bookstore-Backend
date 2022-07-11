a09c1dbc5c3ac3880bcbddce8e0aafb1da6d290b
AIKursat <aynurkursat1999@hotmail.com>
2022-07-11 17:34:48 +0300

    Create errorJSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -54,3 +54,17 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
   
 	return nil
 }
+
+func(app *application) errorJSON(w http.ResponseWriter, err error, status ...int){
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	app.writeJSON(w, statusCode, payload)
+}
