34eb046ca01820d1fec9dedebac7f422f15b7995
AIKursat <aynurkursat1999@hotmail.com>
2022-07-12 22:21:13 +0300

    Create Token model

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,4 +10,16 @@ type User struct {
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Token     Token     `json:"token"`
+}
+
+type Token struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"-"` // because not getting send that
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Expiry    time.Time `json:"expiry"`
 }
