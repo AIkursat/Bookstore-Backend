f898d16eba8c910bea21e0ffb83af5c184e9bfac
AIKursat <aynurkursat1999@hotmail.com>
2022-07-13 22:44:50 +0300

    Write Update and Delete methods

--- internal/data/models.go
+++ internal/data/models.go
@@ -118,6 +118,51 @@ func (u *User) GetOne(id int) (*User, error) {
 	return &user, nil
 }
 
+func (u *User) Update() error{
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	// statement, because it's not query
+	stmt := `update users set 
+	email = $1,
+	first_name = $2,
+    last_name = $3,
+	updated_at = $4,
+	where id = $5 
+
+	`
+
+	_, err := db.ExecContext(ctx, stmt,
+	u.Email,
+	u.FirstName,
+    u.LastName,
+	time.Now(),
+	u.ID,
+	)
+
+	if err != nil{
+		return err
+	}
+
+	return nil 
+}
+
+func (u *User) Delete() error{
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+    stmt := `delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, u.ID)
+	if err != nil{
+		return err
+	}
+
+	return nil 
+
+}
+
+
 type Token struct {
 	ID        int       `json:"id"`
 	UserID    int       `json:"user_id"`
