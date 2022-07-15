9eb7f4e0cd199310f2ee0ce942cc99874f9ea372
AIKursat <aynurkursat1999@hotmail.com>
2022-07-15 17:33:14 +0300

    Insert and Delete Token

--- internal/data/models.go
+++ internal/data/models.go
@@ -369,3 +369,51 @@ func (t *Token) AuthenticateToken(r *http.Request) (*User, error){
 
 }
 
+func (t *Token) Insert(token Token, u User) error{
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+    // delete existing tokens
+	stmt := `delete	from tokens where user_id = $1`
+	_, err := db.ExecContext(ctx, stmt, token.UserID)
+	if err != nil{
+		return err
+	}
+
+	token.Email = u.Email
+
+	stmt = `insert into tokens (user_id, email, token, token_hash, created_at, updated_at, expiry
+		values($1, $2, $3, $4, $5, $6, $7)`
+
+        // we can't use := becauuse we already have err
+		_, err =db.ExecContext(ctx, stmt,
+			token.UserID,
+			token.Email,
+			token.Token,
+			token.TokenHash,
+			time.Now(),
+			time.Now(),
+			token.Expiry,			
+			) 
+
+			if err != nil{
+				return err
+			}
+
+		return nil
+
+}
+
+func (t *Token) DeleteByToken(plainText string) error{
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from tokens where token = $1`
+
+	_, err := db.ExecContext(ctx, stmt, plainText)
+
+	if err != nil{
+		return err
+	}
+	return nil
+}
