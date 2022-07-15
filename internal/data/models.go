12d441dee97b95f2c596355c9c9d416ec97862e2
AIKursat <aynurkursat1999@hotmail.com>
2022-07-15 16:36:32 +0300

    Generate and authenticate a token

--- internal/data/models.go
+++ internal/data/models.go
@@ -2,9 +2,13 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base32"
 	"errors"
-	"go/token"
+	"math/rand"
+	"net/http"
+	"strings"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -302,3 +306,66 @@ func(t *Token) GetUserForToken(token Token) (*User, error) {
     return &user, nil
 
 }
+
+func (t *Token) GenerateToken(UserID int, ttl time.Duration) (*Token, error){ // ttl means "time to life"
+     
+	token := &Token {
+		UserID: UserID,
+		Expiry: time.Now().Add(ttl),
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+    
+	if err != nil{
+		return nil, err
+	}
+    
+	// if we pass the error
+
+	// that gives us the actual token itself
+	token.Token = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+    hash := sha256.Sum256([]byte(token.Token))
+	token.TokenHash = hash[:] // converting
+
+	return token, nil
+
+}
+
+func (t *Token) AuthenticateToken(r *http.Request) (*User, error){
+   authorizationHeader := r.Header.Get("Authorization")
+   if authorizationHeader == "" {
+	return nil, errors.New("no authorization header received") // if there is no user
+   }
+
+   headerParts := strings.Split(authorizationHeader, " ")
+   if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+	return nil, errors.New("no valid authorization header received") 	
+   }
+
+   token := headerParts[1]
+
+   if len(token) != 26 {
+	return nil, errors.New("token wrong size") 
+   }
+
+   tkn, err := t.GetByToken(token) // token model form from db
+   if err != nil{
+	return nil, errors.New("no matching token found") 
+   }
+
+   if tkn.Expiry.Before(time.Now()){
+	return nil, errors.New("Expired Token") 
+   }
+
+   user, err := t.GetUserForToken(*tkn)
+   if err != nil{
+	return nil, errors.New("no matching user found") 
+   }
+
+   return user, nil
+
+}
+
